a57a7e795e745f6fa7e1383a92c0812a84ae83da
anan <f_thex@hotmail.com>
2017-12-20 10:15:43 +0700

    t

---
+++ Phoneword/Phoneword/PhonewordTranslator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phoneword
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
+        }
+    }
+}
